947fb51688ecaa65afe12d95ebf56734af6831fb
Zarni Ohn <Zarni Ohn>
2016-02-11 19:09:05 -0500

    edit index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-  print("Hello World");
-?>
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <title>About Me</title>
+	<link href='bootstra-3.3.6-dist/css/bootstrap.css' rel='stylesheet'>
+    </head>
+    <body>
+    	<h1>Zarni Ohn</h1>
+	<h2>Information Technology (Game Development)</h2>
+	<p>
+
+	</p>
