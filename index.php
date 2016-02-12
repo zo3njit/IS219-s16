9fbd559d54de19cb8248e3d69f4da02daa52acba
Zarni Ohn <Zarni Ohn>
2016-02-11 19:16:55 -0500

    add bootstrap

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang='en'>
     <head>
         <title>About Me</title>
-	<link href='bootstra-3.3.6-dist/css/bootstrap.css' rel='stylesheet'>
+	<link href='bootstrap-3.3.6-dist/css/bootstrap.css' rel='stylesheet'>
     </head>
     <body>
     	<h1>Zarni Ohn</h1>
@@ -10,3 +10,7 @@
 	<p>
 
 	</p>
+
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+    <script src='bootstrap-3.3.6-dist/js/bootstrap.js'></script>
+</html>
