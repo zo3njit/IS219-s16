9a19209c978b5aacd4d8e86b5e12f90127ccaa0a
Zarni Ohn <Zarni Ohn>
2016-02-15 18:02:09 -0500

    edit index.php

--- index.php
+++ index.php
@@ -9,7 +9,10 @@
 	<h2>Information Technology (Game Development)</h2>
 	    <div class='container-fluid'>
 	    	<p>
-	            My name is Zarni Ohn and I am a student here at NJIT. I am a Junior that t			  ransfered over from the Computer Science program. I switched over to Infor			mation Technology because of the Game Development Program. I have always b		      een interested in games and would like to pursue a career in creating game		    s that I think many others like myself would enjoy.
+	            My name is Zarni Ohn and I am a student here at NJIT. I am a Junior that transfered over from 
+				the Computer Science program. I switched over to Information Technology because of the Game 
+				Development Program. I have always been interested in games and would like to pursue a career 
+				in creating games that I think many others like myself would enjoy.
 	    	</p>
 	    </div>
 
