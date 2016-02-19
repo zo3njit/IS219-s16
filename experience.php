21b14a208ebc970bce391c640e422ef12c33b4dd
zo3njit <zo3@njit.edu>
2016-02-19 03:57:23 -0500

    Added files
    Added experience and education PHP files

---
+++ experience.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	 
+    <title>zo3-Project1-IS219102</title>
+	
+	
+	<link href='css/bootstrap-theme.css' rel='stylesheet'>
+	
+</head>
+
+<body>
+	
+	<nav class="fixed_top_navbar">
+		<div class="container">
+			
+		
+		</div>
+	
+	
+	
+	
+	</nav>
+	
+	
+
+
+	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+    <script src='bootstrap-3.3.6-dist/js/bootstrap.js'></script>
+</body>	
+
+</html>
--- index.php
+++ index.php
@@ -1,21 +1,94 @@
 <!DOCTYPE html>
-<html lang='en'>
-    <head>
-        <title>About Me</title>
-	<link href='bootstrap-3.3.6-dist/css/bootstrap.css' rel='stylesheet'>
-    </head>
-    <body>
-    	<h1>Zarni Ohn</h1>
-	<h2>Information Technology (Game Development)</h2>
-	    <div class='container-fluid'>
-	    	<p>
-	            My name is Zarni Ohn and I am a student here at NJIT. I am a Junior that transfered over from 
-				the Computer Science program. I switched over to Information Technology because of the Game 
-				Development Program. I have always been interested in games and would like to pursue a career 
-				in creating games that I think many others like myself would enjoy.
-	    	</p>
-	    </div>
+<html lang="en">
 
-    <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
-    <script src='bootstrap-3.3.6-dist/js/bootstrap.js'></script>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bare - Start Bootstrap Template (for IS219)</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <style>
+    body {
+        padding-top: 70px;
+        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+    }
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Start Bootstrap</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>A Bootstrap Starter Template for IS219</h1>
+                <p class="lead">Complete with pre-defined file paths that you won't have to change!</p>
+                <ul class="list-unstyled">
+                    <li>Bootstrap v3.3.6</li>
+                    <li>jQuery v1.11.1</li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
