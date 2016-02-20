079862d4f5036f3637144b08645e52b1751e36c1
zo3njit <zo3@njit.edu>
2016-02-19 19:10:57 -0500

    change stylesheet
    test

--- experience.php
+++ experience.php
@@ -2,32 +2,59 @@
 <html lang="en">
 
 <head>
-	 
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="zo3project1is219102">
+	<meta name="author" content="Zarni Ohn">
+	
     <title>zo3-Project1-IS219102</title>
 	
-	
-	<link href='css/bootstrap-theme.css' rel='stylesheet'>
+	<link href='css/bootstrap.css' rel='stylesheet'>
+	<link href='css/custom.css' rel='stylesheet'>
 	
 </head>
 
 <body>
 	
-	<nav class="fixed_top_navbar">
+	<nav class="navbar">
 		<div class="container">
+			<div class="navbar_title">
+				<a class="navbar_logo">
+			</div>		
 			
-		
+			<div class="navbar-default">
+				<ul class="navbar_unordered_list">
+					<a class="navbar_Home" href="index.php">Home</a>
+					<a class="navbar_Home" href="experience.php">Work Experience</a>
+					<a class="navbar_Home" href="education.php">Education</a>
+				</ul>
+			</div>
 		</div>
-	
+		
 	
 	
 	
 	</nav>
 	
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h1>Work Experience</h1>
+				
+				
+				
+			</div>
+			<ul class="footer">
+				<li>Bootstrap v3.3.6</li>
+                <li>jQuery v1.11.1</li>
+			</ul>
+		</div>
+	</div>
 
 
 	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
-    <script src='bootstrap-3.3.6-dist/js/bootstrap.js'></script>
+    <script src='js/bootstrap.js'></script>
 </body>	
 
 </html>
--- index.php
+++ index.php
@@ -10,38 +10,52 @@
 	
     <title>zo3-Project1-IS219102</title>
 	
-	<link href='css/bootstrap.css' rel='stylesheet'>
+	<link href='css/bootstrap.min.css' rel='stylesheet'>
+	<link href='css/custom.css' rel='stylesheet'>
 	
 </head>
 
 <body>
 	
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
-			<div class="navbar">
-				<a class=
-			</div>		
-			
+	<nav class="navbar">
+		<div class="container">	
 			<div class="navbar-default">
-				<a class="navbar-brand" href="index.php">Project 1</a>
+				<ul class="navbar_unordered_list">
+					<li><a class="nav_settings" href="index.php">Home</a></li>
+					<li><a class="nav_settings" href="experience.php">Work Experience</a></li>
+					<li><a class="nav_settings" href="education.php">Education</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	
+	
+	
+	<div class="page">
+		<div class="title">
+			<h1>Welcome</h1>				
+		</div>
+		
+		<div class="container"
+			<div>
+				<p class="post">
+					My name is Zarni.
+				</p>
 			</div>
 		</div>
 		
-	
-	
-	
-	</nav>
-	
-	<div class="page">
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<h1>Welcome to my website!</h1>
-				
-				
-				
+		
+		<div class="container">
+			<div class="footer">
+				<p>
+					Bootstrap v3.3.6<br>
+					jQuery v1.11.1
+				</p>
 			</div>
 		</div>
 	</div>
+	
 
 
 	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
